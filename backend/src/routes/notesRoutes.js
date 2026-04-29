3cc576061aadf5679777a6e4d4dea7bf215252c6
Maximilian Schöpf <172821797+Peter-Pander@users.noreply.github.com>
2026-04-29 22:05:47 +0200

    Complete note controllers

--- backend/src/routes/notesRoutes.js
+++ backend/src/routes/notesRoutes.js
@@ -1,6 +1,8 @@
 import express from "express";
+
 import {
   getAllNotes,
+  getNoteById,
   createNote,
   updateNote,
   deleteNote,
@@ -9,6 +11,7 @@ import {
 const router = express.Router();
 
 router.get("/", getAllNotes);
+router.get("/:id", getNoteById);
 router.post("/", createNote);
 router.put("/:id", updateNote);
 router.delete("/:id", deleteNote);
